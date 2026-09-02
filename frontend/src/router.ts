28bd22c9fc8c4451615781634a03fb3cddb45ba1
kusdaviid <dkusnezov93@gmail.com>
2026-09-02 09:18:26 +0200

    fix: Terminalnummer aus der Route übergeben

--- frontend/src/router.ts
+++ frontend/src/router.ts
@@ -4,5 +4,7 @@ import TerminalView from './views/TerminalView.vue'
 
 export default createRouter({
   history: createWebHistory(),
-  routes: [{ path: '/terminal/:terminalId', component: TerminalView }]
+  routes: [
+    { path: '/terminal/:terminalId', component: TerminalView, props: true }
+  ]
 })
